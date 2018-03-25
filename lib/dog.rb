069502c2d350ab77cb625a780ff8bf6116898323
Antonio Reid <read.musik@gmail.com>
2018-03-25 20:00:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,6 +66,10 @@ class Dog
     dog = self.new(attributes)
   end
 
+  def self.find_by_name(name)
+    
+  end
+
   def self.find_or_create_by(params)
     sql = <<-SQL
       SELECT * FROM dogs WHERE name = ? AND breed = ?
