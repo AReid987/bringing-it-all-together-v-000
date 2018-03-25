1fc7ecd15d5fe6c19b307ca06ec7c015ba07a646
Antonio Reid <read.musik@gmail.com>
2018-03-25 20:02:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,11 @@ class Dog
   end
 
   def self.find_by_name(name)
-    
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name)
   end
 
   def self.find_or_create_by(params)
